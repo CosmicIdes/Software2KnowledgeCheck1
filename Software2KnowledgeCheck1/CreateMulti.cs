250291a7afcf29a32e9a32afe076776278326bc3
Brandi Hornbuckle <brandi.hornbuckle@gmail.com>
2023-06-16 15:38:55 -0400

    Refactored City.cs, and created a generic class that can be used on Apartments or High Rises.

---
+++ Software2KnowledgeCheck1/CreateMulti.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace Software2KnowledgeCheck1
+{
+	public class CreateMulti<T>
+	{
+        public void CreateMultifloor(T multi)
+        {
+            // Get materials
+            var materialRepo = new MaterialsRepo();
+            var materialsNeeded = materialRepo.GetMaterials();
+
+            var permitRepo = new ZoningAndPermitRepo();
+
+            var buildingWasMade = ConstructBuilding<Apartment>(materialsNeeded, permitRepo.GetPermit(), permitRepo.ZoningApproves());
+
+            if (buildingWasMade)
+            {
+                Buildings.Add(multi);
+            }
+        }
+
+        public bool ConstructBuilding<T>(List<string> materials, bool permit, bool zoning) where T : Building
+        {
+            if (permit && zoning)
+            {
+                foreach (var material in materials)
+                {
+                    if (material == "concrete")
+                    {
+                        // start laying foundation
+                    }
+                    else if (material == "Steel")
+                    {
+                        // Start building structure
+                    }
+                    // etc etc...
+
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
+
